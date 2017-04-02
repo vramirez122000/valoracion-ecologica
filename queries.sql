3ade9f02e94c1550b01b9dd8d2cf1733961eadec
vramirez122000 <bitingo80>
2017-04-02 13:02:22 -0400

    valoracion, natural jenks

--- queries.sql
+++ queries.sql
@@ -1,11 +1,13 @@
-create extension tablefunc;
+select gid, unnest(services) from poli_services where gid = 31;
 
-create table total_values as
+
+--create table total_values as
 WITH tmp AS (
     SELECT
       poli_services.gid,
       unnest(poli_services.services) srv_id
     FROM poli_services
+    where gid = 31
 ), tmp2 AS (
     SELECT
       p.gid,
@@ -14,23 +16,22 @@ WITH tmp AS (
       srv.ecosystem,
       srv.service,
       srv.subservice,
-      st_area(geom) / 10000                      area_ha,
+      st_area(geom) / 10000 area_ha,
       (st_area(geom) / 10000) * srv.query_values assess_val_usd,
       p.geom
     FROM landuse p
       JOIN tmp ON p.gid = tmp.gid
       JOIN servicios srv ON tmp.srv_id = srv.unique_id
+
 ) select
-    gid,
-    landuse,
+    --gid,
+    --landuse,
     sum(assess_val_usd)  total_assess_val,
     sum(area_ha) area_ha,
-    sum(area_ha) * 10000 / 3980 area_cda,
-    sum(assess_val_usd) / sum(area_ha) usd_per_ha,
-    sum(assess_val_usd) / (sum(area_ha) * 10000 / 3980 ) usd_per_cda,
-    geom
+    sum(assess_val_usd) / sum(area_ha) usd_per_ha
+    --geom
   from tmp2
-  group by gid, landuse, geom
+  --group by gid, landuse, geom
 ;
 
 
